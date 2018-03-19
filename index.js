dab8efd4531201d9cfd7a63dddbb82ec69eee8d1
binit katiyar <binit.katiyar@gmail.com>
2018-03-19 17:34:33 +0530

    finished oauth flow

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-// app: Express app to register this route handler with
-// get: Watch for incoming requests with this method
-// /: Watch for requests trying to access /
-// req: Object representing the incoming request
-// res: Object representing the outgoing response
-// res:send(): Immediately send some JSON back to who ever made this request
-// listen(5000): express telling to node that watch to the any traffic comming from port 5000 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey] //This is for automatic encryption of data
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-    res.send({ message: 'build deployment' });
-});
+require('./routes/authRoutes')(app);
+
+/* app: Express app to register this route handler with
+get: Watch for incoming requests with this method
+/: Watch for requests trying to access /
+req: Object representing the incoming request
+res: Object representing the outgoing response
+res:send(): Immediately send some JSON back to who ever made this request
+listen(5000): express telling to node that watch to the any traffic comming from port 5000  */
 
 // Look at the underlying environment that see if they have declared a port for us to use.
 const PORT = process.env.PORT || 5000; 
---
+++ routes/authRoutes.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.send({ message: 'build deployment' });
+    });
+    
+    app.get(
+        '/auth/google', 
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    );
+    
+    app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google')
+    );
+
+    app.get('/api/logout', (req, res) => {
+        //logout is function automatically attached with req object by passport. It kills the cookies
+        req.logout();
+        res.send(req.user);
+        // as soon as req.logout() will get executed, passport will automatically destroyed user object associated with req object
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        //res.send(req.session);
+        res.send(req.user);
+    });
+};
